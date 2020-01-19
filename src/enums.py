1a9effb19d2d6f10d29a7f60c4123f9d38c6cb9e
Amael Tardif <amael.tardif@epita.fr>
2020-01-19 03:09:25 +0100

    parser: add none types

--- src/enums.py
+++ src/enums.py
@@ -19,16 +19,19 @@ class Symbol(Enum):
     CARDF = 15
     CARDG = 16
     CARDH = 17
+    EMPTY = 18
 
     def by_rep(this, rep):
         if isinstance(value, str):
+            if value == "_":
+                return this["EMPTY"]
             return this(ord(value) - 87) # 'a' -> 10
         return this(value)
-
 
 class State(Enum):
     EAT = 0
     INCR = 1
+    END = 2
 
     def by_rep(this, rep):
         return this[upper(name[2:])]
@@ -36,6 +39,9 @@ class State(Enum):
 class Direction(Enum):
     RIGHT = 0
     LEFT = 1
+    NONE = 2
 
     def by_rep(this, rep):
+        if rep == "-":
+            return this["NONE"]
         return rep == ">" ? this["RIGHT"] : this["LEFT"]
